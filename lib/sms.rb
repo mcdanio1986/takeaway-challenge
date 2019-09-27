8dc6308a48b2cd3bdca2b3132d9053261d09a932
mcdanio1986 <bartosz.danek@gmail.com>
2019-09-27 15:25:22 +0100

    sms file created -sms already working

---
+++ lib/sms.rb
@@ -0,0 +1,19 @@
+require 'twilio-ruby'
+
+def time
+  add_hour = Time.now + 3600
+  add_hour.strftime("%H:%M")
+end
+# put your own credentials here
+account_sid = 'ACff0050d994728217a2b61e4867dfcc14'
+auth_token = 'c600b69ddf837a77f0ead048a1ba5308'
+
+# set up a client to talk to the Twilio REST API
+@client = Twilio::REST::Client.new account_sid, auth_token
+
+
+@client.messages.create(
+  from: '+441618504736',
+  to: '+447879965073',
+  body: "Hey there!#{time}"
+)
